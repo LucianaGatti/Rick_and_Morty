36b7b5982f0a3be7d2e76db5589cc783e21ffb40
lugatti92 <lugatti92@gmail.com>
2023-05-18 21:32:12 -0300

    error 404 agregado

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import './App.css';
-import Cards from './components/Cards/Cards.jsx';
-import Nav from './components/Nav/Nav';
-import { useState } from 'react';
-import axios from "axios"
-import About from './components/About/About.jsx';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
-import Detail from './components/Detail/Detail'
+import "./App.css";
+import Cards from "./components/Cards/Cards.jsx";
+import Nav from "./components/Nav/Nav";
+import { useState } from "react";
+import axios from "axios";
+import About from "./components/About/About.jsx";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Detail from "./components/Detail/Detail";
+import Error from "./components/Error404/Error";
 
 function App() {
-  const [characters, setCharacters] = useState ([]); 
+  const [characters, setCharacters] = useState([]);
 
   function onSearch(id) {
-  axios(`https://rickandmortyapi.com/api/character/${id}`)
-  .then(({ data }) => {
-    if (data.name && !characters.find(char => char.id ===data.id)) {
-        // setCharacters((oldChars) => [...oldChars, data]);
-        setCharacters([...characters, data])
-    } else {
-        window.alert('¡No hay personajes con este ID!');
-    }
-  });
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+      ({ data }) => {
+        if (data.name && !characters.find((char) => char.id === data.id)) {
+          // setCharacters((oldChars) => [...oldChars, data]);
+          setCharacters([...characters, data]);
+        } else {
+          window.alert("¡No hay personajes con este ID!");
+        }
+      }
+    );
   }
 
   //*filter no modifica el array original, crea uno nuevo.
-const onClose = (id) => {
-  setCharacters(characters.filter((char) => char.id !== id)
-  )
-
-}
+  const onClose = (id) => {
+    setCharacters(characters.filter((char) => char.id !== id));
+  };
   return (
     <div className="App">
-        < Nav onSearch= {onSearch} />
-        {/* <Cards characters={characters} onClose = {onClose} /> */}
+      <Nav onSearch={onSearch} />
+      {/* <Cards characters={characters} onClose = {onClose} /> */}
+      <Switch>
+        <Route exact path="/about" component={About} />
 
-          <Route path="/about" component={About}/>
+        <Route exact path="/detail/:id" component={Detail} />
 
-          <Route path="/detail/:id" component={Detail} />
+        <Route exact path="/home">
+          <Cards characters={characters} onClose={onClose} />
+        </Route>
 
-          <Route path="/home">
-            <Cards characters={characters} onClose = {onClose} />
-          </Route>
-
+        <Route path="*" component={Error} />
+      </Switch>
     </div>
   );
 }
--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -25,15 +25,15 @@ const Detail = () => {
 
     return ( 
 <div className={Style.contain}>
-        <div className={Style.detail}>
+      <div className={Style.detail}>
          <h3 className= {Style.name}>{character.name}</h3>
          <p className= {Style.info}>Status: {character.status}</p>
          <p className= {Style.info}>Species: {character.species}</p>
          <p className= {Style.info}> Gender: {character.gender}</p>
          <p className= {Style.info}> Origin:{character.origin?.name}</p>
+      </div>
          <img className= {Style.img} src={character.image} alt="img" />
-        </div>
-        </div>
+</div>
 
      );
 }
