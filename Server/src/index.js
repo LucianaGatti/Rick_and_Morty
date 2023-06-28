0c480d00125ddf33e85eb062284a121c16848221
lugatti <lugatti92@gmail.com>
2023-06-28 18:22:24 -0300

    homework testing

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,25 +1,26 @@
-const express = require ("express");
-const server = express();
-const router = require ("./routes/index")
+// const express = require ("express");
+// const server = express();
+// const router = require ("./routes/index")
 const PORT = 3001;
+const server = require ("./app")
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header(
+//      'Access-Control-Allow-Headers',
+//      'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.header(
+//      'Access-Control-Allow-Methods',
+//      'GET, POST, OPTIONS, PUT, DELETE'
+//   );
+//   next();
+// });
 
-server.use(express.json());
+// server.use(express.json());
 
-server.use("/rickandmorty", router)
+// server.use("/rickandmorty", router)
 
 server.listen(PORT, ()=> {console.log(`LISTENING ON PORT: ${PORT}`);
 })
