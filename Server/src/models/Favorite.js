e453f531f6f3804b76e3ef934f290063b68f37a0
lugatti <lugatti92@gmail.com>
2023-07-12 18:24:46 -0300

    listo

--- Server/src/models/Favorite.js
+++ Server/src/models/Favorite.js
@@ -4,7 +4,7 @@ module.exports = (sequelize) => {
    sequelize.define('Favorite', {
       id: {
          type: DataTypes.INTEGER,
-         allowNull: true,
+         allowNull: false,
          primaryKey: true,
       },
       name: {
